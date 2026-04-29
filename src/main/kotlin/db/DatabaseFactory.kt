cf69667e5e9af0448de187cd60fbd8275947a186
Оганнисян Нарек Геворгович <narek.ogannisyan.82@bk.ru>
2026-04-29 11:27:13 +0300

    add new server

--- src/main/kotlin/db/DatabaseFactory.kt
+++ src/main/kotlin/db/DatabaseFactory.kt
@@ -9,9 +9,15 @@ import org.jetbrains.exposed.sql.transactions.transaction
 object DatabaseFactory {
     fun init() {
         val driverClassName = "org.postgresql.Driver"
-        val jdbcURL = "jdbc:postgresql://localhost:5433/book_catalog_db"
-        val user = "postgres"
-        val password = "12345"
+        //ВАРИАНТ 1: УДАЛЕННАЯ БАЗА NEON.TECH
+        val jdbcURL = "jdbc:postgresql://ep-noisy-term-al8tjwma-pooler.c-3.eu-central-1.aws.neon.tech/neondb?sslmode=require"
+        val user = "neondb_owner"
+        val password = "npg_wn6ihLVYfS9z"
+
+        //ВАРИАНТ 2: ЛОКАЛЬНАЯ БАЗА (ДЛЯ САМСУНГА)
+//        val jdbcURL = "jdbc:postgresql://localhost:5433/book_catalog_db"
+//        val user = "postgres"
+//        val password = "12345"
 
         // Подключаемся к БД
         val database = Database.connect(jdbcURL, driverClassName, user, password)
